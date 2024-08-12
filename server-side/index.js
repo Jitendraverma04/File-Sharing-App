5f290ded010a94d7da12a40a4d269c4625dca787
Jitendra Verma <168767008+Jitendraverma04@users.noreply.github.com>
2024-08-12 11:21:42 +0530

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -13,6 +13,14 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://file-sharing-app-iota-five.vercel.app'); // replace with your frontend URL
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', router);
