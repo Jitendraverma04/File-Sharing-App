9656b1175098e9dd4e4811fe35816dc45480bb02
Jitendra Verma <168767008+Jitendraverma04@users.noreply.github.com>
2024-08-12 10:53:55 +0530

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -8,7 +8,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:["https://filesharewithmefront-end.vercel.app"],
+  method:["GET","POST"],
+  credentials:true
+  }
+));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/', router);
