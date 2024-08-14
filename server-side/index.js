af3dc4b83a36ea8b19a032bd7fb8d8260aeb08cf
Jitendra Verma <168767008+Jitendraverma04@users.noreply.github.com>
2024-08-14 13:29:48 +0530

    Update index.js

--- server-side/index.js
+++ server-side/index.js
@@ -14,12 +14,12 @@ const app = express();
 //   credentials: true
 // }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://file-sharing-app-iota-five.vercel.app'); // replace with your frontend URL
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://file-sharing-app-iota-five.vercel.app'); // replace with your frontend URL
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   next();
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
